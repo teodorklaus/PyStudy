a9afd037e5f75331a767f7e8b18fa954ad85a9e4
teodorklaus <sergey.gorshok@gmail.com>
2017-01-10 23:24:41 +0200

    last

---
+++ da1.HW.4.2(File2).py
@@ -0,0 +1,9 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+#max(num1,num2,num3)
+#min(num1,num2,num3)
+
+print ("max: " + str(max(num1,num2,num3)))
+print ("max: " + str(min(num1,num2,num3)))
