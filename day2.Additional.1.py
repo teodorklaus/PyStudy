9ff291c5f4f66d369d9867d1846fc7675d840055
teodorklaus <sergey.gorshok@gmail.com>
2017-01-11 18:02:24 +0200

    End work with task

---
+++ day2.Additional.1.py
@@ -0,0 +1,5 @@
+a = int(input('Enter hypotenuse: '))
+b = int(input('Enter legs1: '))
+c = int(input('Enter legs2: '))
+
+print ('Rectangular triangle' if a * a == b * b + c * c else 'Triangle is not Rectangular')
---
+++ day2.Additional.2.py
@@ -0,0 +1,8 @@
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+
+print ('num1: ' + str(num1 ** 3 if num1 > 0 else num1 * 0))
+print ('num2: ' + str(num2 ** 3 if num2 > 0 else num2 * 0))
+print ('num3: ' + str(num3 ** 3 if num3 > 0 else num3 * 0))
