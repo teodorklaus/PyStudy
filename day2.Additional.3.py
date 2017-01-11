2a6fd96356208244a71905b0023fc76b8304a3e0
teodorklaus <sergey.gorshok@gmail.com>
2017-01-11 20:15:35 +0200

    Rework

--- day2.Additional.3.py
+++ day2.Additional.3.py
@@ -1,6 +1,9 @@
-num = int(input('Enter number: '))
+num = int(input('Enter 3 numeral: '))
 
-list1 = list str(num)
-end = (list1.count)
-print (end)
-
+list1 = list (str(num))
+if list1[0] == list1[1] and list1[1] == list1[2]:
+    print ('Entered number has 3 the same numeral: ' + list1[1])
+elif list1[0] == list1[1] or list1[1] == list1[2] or list1[0] == list1[2]:
+    print ('Entered number has 2 the same numeral!')
+elif list1[0] != list1[1] and list1[1] != list1[2] and list1[0] != list1[2]:
+    print ('Entered number didnot have the same numeral: ' + str(list1))
