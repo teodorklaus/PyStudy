343133ea1300bf25559fdcf7dd5c22db07a0dacb
teodorklaus <sergey.gorshok@gmail.com>
2017-01-11 20:47:53 +0200

    End work with this task

--- day2.Additional.6.py
+++ day2.Additional.6.py
@@ -1,12 +1,7 @@
-a = int(input('Enter numeral A: '))
-b = int(input('Enter numeral B: '))
-c = int(input('Enter numeral C: '))
 
-if a == b:
-    print ('A = B')
-elif b == c:
-    print ('B = C')
-elif a == c:
-    print ('A = C')
-else:
-    print ('A, B, C: dinot have couple.')
+speed = float(input('Enter you speed: '))
+distance = float(input('Enter you distance to target: '))
+
+time = distance / speed
+print ('With this speed you reach the target ' + str(time) + ' Hour')
+
